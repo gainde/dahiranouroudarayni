1c859990cd1617e480aaedb95e954f27ee9a3bc6
Moussa Thimbo <moussa.thimbo@gmail.com>
2014-03-17 14:04:10 -0400

    ajout de la classe adresse

---
+++ src/classes/Adresse.java
@@ -0,0 +1,56 @@
+package classes;
+
+public class Adresse {
+	private String rue;
+	private String ville;
+	private String province;
+	private String codepostale;
+	private String pays;
+	
+	public Adresse(){
+		
+	}
+
+	public String getRue() {
+		return rue;
+	}
+
+	public String getVille() {
+		return ville;
+	}
+
+	public String getProvince() {
+		return province;
+	}
+
+	public String getCodepostale() {
+		return codepostale;
+	}
+
+	public String getPays() {
+		return pays;
+	}
+
+	public void setRue(String rue) {
+		this.rue = rue;
+	}
+
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+
+	public void setProvince(String province) {
+		this.province = province;
+	}
+
+	public void setCodepostale(String codepostale) {
+		this.codepostale = codepostale;
+	}
+
+	public void setPays(String pays) {
+		this.pays = pays;
+	}
+	
+	
+
+}
