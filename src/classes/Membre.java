b92fa1226c9991278c888745d3a67446630ad78f
Moussa Thimbo <moussa.thimbo@gmail.com>
2014-03-17 13:35:53 -0400

    init projet

---
+++ src/classes/Membre.java
@@ -0,0 +1,14 @@
+package classes;
+
+public class Membre {
+	private String nom;
+	private String prenom;
+	private String dateNaissance;
+	private String telephone;
+	private String email;
+	
+	public Membre(){
+		
+	}
+
+}
