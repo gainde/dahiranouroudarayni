7d920ce9e3889a2f29cfd2cc9968817b822910da
gainde <moussa.thimbo@gmail.com>
2014-05-14 04:31:38 -0400

    add classpath
    ajout class path pour tree project

--- src/logique/Main.java
+++ src/logique/Main.java
@@ -1,11 +1,18 @@
 package logique;
 
+import javafx.application.Application;
+import javafx.cotisation.CotisationView;
+import javafx.stage.Stage;
+
 public class Main {
 	
 	public static  void main(String args[]){
 		//Test
-		Authentification auth = new Authentification(null, true);
-		auth.setVisible(true);
+		/*Authentification auth = new Authentification(null, true);
+		auth.setVisible(true);*/
+		//sample s = new sample();
+		//Application.launch(sample.class);
+		
 	}
 	
 }
