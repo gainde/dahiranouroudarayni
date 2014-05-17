8ad320dd4c4cdaca1b2d4ca6ec475b234c2cd763
fallouseven <oussou.dieng@gmail.com>
2014-05-17 16:30:01 -0400

    Retouche interface integration de la librairie itext pour generer pdf

--- src/logique/Main.java
+++ src/logique/Main.java
@@ -1,5 +1,14 @@
 package logique;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Date;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+import com.itextpdf.text.pdf.codec.Base64.OutputStream;
+
 import javafx.application.Application;
 import javafx.membre.Membres;
 import javafx.membre.ajout.AjouterMembres;
@@ -21,6 +30,19 @@ public class Main {
 		//Authentification auth = new Authentification(null, true);
 		//.setVisible(true);
 		Application.launch(Membres.class);
+		/*
+		try {
+            FileOutputStream file = new FileOutputStream(new File("Test.pdf"));
+            Document document = new Document();
+            PdfWriter.getInstance(document, file);
+            document.open();
+            document.add(new Paragraph("Hello World, iText"));
+            document.add(new Paragraph(new Date().toString()));
+            document.close();
+            file.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }*/
 		
 	}
 	
