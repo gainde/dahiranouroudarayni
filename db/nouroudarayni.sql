eb1bac2f94df9a1b39fcaf25af2cdc13daec4f36
gainde <moussa.thimbo@gmail.com>
2014-05-22 21:27:15 -0400

    database

--- db/nouroudarayni.sql
+++ db/nouroudarayni.sql
@@ -1,27 +1,34 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.9
+-- version 4.0.4
 -- http://www.phpmyadmin.net
 --
--- Client :  localhost:8889
--- Généré le :  Jeu 22 Mai 2014 à 21:15
--- Version du serveur :  5.5.34
--- Version de PHP :  5.5.10
+-- Client: localhost
+-- Généré le: Ven 23 Mai 2014 à 01:25
+-- Version du serveur: 5.6.12-log
+-- Version de PHP: 5.4.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
---
--- Base de données :  `nouroudarayni`
---
 
--- --------------------------------------------------------
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données: `nouroudarayni`
+--
 CREATE DATABASE IF NOT EXISTS `nouroudarayni` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `nouroudarayni`;
+
+-- --------------------------------------------------------
+
 --
 -- Structure de la table `cotisationdiverse`
 --
 
-CREATE TABLE `cotisationdiverse` (
+CREATE TABLE IF NOT EXISTS `cotisationdiverse` (
   `montant` int(11) NOT NULL,
   `datecotisation` datetime NOT NULL,
   `commentaire` text NOT NULL
@@ -33,7 +40,7 @@ CREATE TABLE `cotisationdiverse` (
 -- Structure de la table `cotisationevenement`
 --
 
-CREATE TABLE `cotisationevenement` (
+CREATE TABLE IF NOT EXISTS `cotisationevenement` (
   `id` int(11) NOT NULL,
   `montant` double NOT NULL,
   `datecotisation` datetime NOT NULL,
@@ -52,7 +59,17 @@ INSERT INTO `cotisationevenement` (`id`, `montant`, `datecotisation`, `idEven`, 
 (1353, 75, '2014-04-22 00:00:00', 'Autre', 'address@gmail.com'),
 (1354, 50, '2013-04-10 00:00:00', 'Autre', 'address@gmail.com'),
 (1366, 100, '2014-04-01 00:00:00', 'Autre', 'test@gmail.com'),
-(1403, 450, '2014-05-13 00:00:00', 'Autre', 'test@gmail.com');
+(1403, 450, '2014-05-13 00:00:00', 'Autre', 'test@gmail.com'),
+(1756, 455, '2013-05-16 00:00:00', 'Autre', 'aba@gmail.com'),
+(1759, 100, '2013-05-20 00:00:00', 'Autre', 'ava@gmail.ca'),
+(1763, 45, '2014-05-15 00:00:00', 'Autre', 'bayess@gmail.com'),
+(1766, 544, '2013-05-15 00:00:00', 'Autre', 'bdd@gmail.com'),
+(1769, 500, '2013-01-15 00:00:00', 'Autre', 'ffss@gmail.com'),
+(1773, 212, '2013-05-21 00:00:00', 'Autre', 'idi@gmail.com'),
+(1778, 200, '2014-05-08 00:00:00', 'Autre', 'moussa.thimbo@gmail.com'),
+(1781, 52, '2013-05-15 00:00:00', 'Autre', 'moussa.thimbo@hotmail.com'),
+(1786, 500, '2013-05-23 00:00:00', 'Autre', 'oumar@hotmail.ca'),
+(1789, 200, '2014-05-01 00:00:00', 'Autre', 'oussou.dieng@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -60,7 +77,7 @@ INSERT INTO `cotisationevenement` (`id`, `montant`, `datecotisation`, `idEven`, 
 -- Structure de la table `cotisationkst`
 --
 
-CREATE TABLE `cotisationkst` (
+CREATE TABLE IF NOT EXISTS `cotisationkst` (
   `id` int(11) NOT NULL,
   `type` varchar(1) NOT NULL DEFAULT '0',
   `montant` double NOT NULL,
@@ -80,7 +97,17 @@ INSERT INTO `cotisationkst` (`id`, `type`, `montant`, `datecotisation`, `idMembr
 (1362, '1', 50, '2014-05-08', 'address@gmail.com'),
 (1363, '1', 50, '2014-04-08', 'address@gmail.com'),
 (1364, '1', 50, '2014-03-11', 'address@gmail.com'),
-(1365, '0', 500, '2013-03-06', 'address@gmail.com');
+(1365, '0', 500, '2013-03-06', 'address@gmail.com'),
+(1755, '1', 45, '2013-05-08', 'aba@gmail.com'),
+(1758, '1', 65, '2013-04-12', 'ava@gmail.ca'),
+(1762, '1', 54, '2013-03-20', 'bayess@gmail.com'),
+(1765, '1', 44, '2013-05-15', 'bdd@gmail.com'),
+(1768, '1', 44, '2014-05-15', 'ffss@gmail.com'),
+(1771, '1', 45, '2013-05-23', 'idi@gmail.com'),
+(1772, '1', 45, '2013-03-04', 'idi@gmail.com'),
+(1777, '1', 500, '2013-05-07', 'moussa.thimbo@gmail.com'),
+(1780, '1', 54, '2013-04-10', 'moussa.thimbo@hotmail.com'),
+(1785, '1', 50, '2013-03-12', 'oumar@hotmail.ca');
 
 -- --------------------------------------------------------
 
@@ -88,9 +115,9 @@ INSERT INTO `cotisationkst` (`id`, `type`, `montant`, `datecotisation`, `idMembr
 -- Structure de la table `cotisationmembre`
 --
 
-CREATE TABLE `cotisationmembre` (
+CREATE TABLE IF NOT EXISTS `cotisationmembre` (
   `id` int(11) NOT NULL COMMENT 'id cotisation membre',
-  `montant` int(11) NOT NULL,
+  `montant` double NOT NULL,
   `datecotisation` date NOT NULL,
   `idMembre` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -107,7 +134,25 @@ INSERT INTO `cotisationmembre` (`id`, `montant`, `datecotisation`, `idMembre`) V
 (1357, 50, '2012-05-08', 'address@gmail.com'),
 (1358, 60, '2012-05-08', 'address@gmail.com'),
 (1359, 60, '2013-09-18', 'address@gmail.com'),
-(1402, 12, '2014-05-14', 'test@gmail.com');
+(1402, 12, '2014-05-14', 'test@gmail.com'),
+(1752, 20, '2013-02-28', 'aba@gmail.com'),
+(1753, 20, '2013-03-07', 'aba@gmail.com'),
+(1754, 45, '2013-04-10', 'aba@gmail.com'),
+(1757, 12, '2013-05-07', 'ava@gmail.ca'),
+(1760, 65, '2013-05-08', 'bayess@gmail.com'),
+(1761, 10, '2013-02-07', 'bayess@gmail.com'),
+(1764, 64, '2013-04-18', 'bdd@gmail.com'),
+(1767, 44, '2013-05-16', 'ffss@gmail.com'),
+(1770, 20, '2013-05-29', 'idi@gmail.com'),
+(1774, 44, '2013-05-15', 'moussa.thimbo@gmail.com'),
+(1775, 46, '2013-04-22', 'moussa.thimbo@gmail.com'),
+(1776, 20, '2013-03-07', 'moussa.thimbo@gmail.com'),
+(1779, 40, '2013-01-17', 'moussa.thimbo@hotmail.com'),
+(1782, 50, '2013-05-01', 'oumar@hotmail.ca'),
+(1783, 40, '2013-04-02', 'oumar@hotmail.ca'),
+(1784, 60, '2013-03-05', 'oumar@hotmail.ca'),
+(1787, 50, '2013-05-20', 'oussou.dieng@gmail.com'),
+(1788, 50, '2013-03-06', 'oussou.dieng@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -115,7 +160,7 @@ INSERT INTO `cotisationmembre` (`id`, `montant`, `datecotisation`, `idMembre`) V
 -- Structure de la table `dahira`
 --
 
-CREATE TABLE `dahira` (
+CREATE TABLE IF NOT EXISTS `dahira` (
   `nom` varchar(100) NOT NULL,
   `description` text NOT NULL,
   `numeroEnregistrement` varchar(100) NOT NULL,
@@ -143,7 +188,7 @@ INSERT INTO `dahira` (`nom`, `description`, `numeroEnregistrement`, `telephone`,
 -- Structure de la table `evenement`
 --
 
-CREATE TABLE `evenement` (
+CREATE TABLE IF NOT EXISTS `evenement` (
   `nom` varchar(100) NOT NULL,
   `budget` decimal(10,0) NOT NULL,
   `depense` decimal(10,0) DEFAULT NULL,
@@ -156,10 +201,10 @@ CREATE TABLE `evenement` (
 --
 
 INSERT INTO `evenement` (`nom`, `budget`, `depense`, `dateEvent`) VALUES
-('Thiant2011', 7000, NULL, '2011-05-01 00:00:00'),
-('Thiant2012', 4000, NULL, '2012-05-01 00:00:00'),
-('thiant2013', 5000, NULL, '2012-05-10 00:00:00'),
-('Thiant2014', 41000, 40000, '2014-04-30 00:00:00');
+('Thiant2011', '7000', NULL, '2011-05-01 00:00:00'),
+('Thiant2012', '4000', NULL, '2012-05-01 00:00:00'),
+('thiant2013', '5000', NULL, '2012-05-10 00:00:00'),
+('Thiant2014', '41000', '40000', '2014-04-30 00:00:00');
 
 -- --------------------------------------------------------
 
@@ -167,7 +212,7 @@ INSERT INTO `evenement` (`nom`, `budget`, `depense`, `dateEvent`) VALUES
 -- Structure de la table `membre`
 --
 
-CREATE TABLE `membre` (
+CREATE TABLE IF NOT EXISTS `membre` (
   `nom` varchar(45) DEFAULT NULL,
   `prenom` varchar(45) DEFAULT NULL,
   `datenaissance` date DEFAULT NULL,
@@ -223,7 +268,7 @@ INSERT INTO `membre` (`nom`, `prenom`, `datenaissance`, `adresse`, `ville`, `pro
 -- Structure de la table `sequence`
 --
 
-CREATE TABLE `sequence` (
+CREATE TABLE IF NOT EXISTS `sequence` (
   `SEQ_NAME` varchar(50) NOT NULL,
   `SEQ_COUNT` decimal(38,0) DEFAULT NULL,
   PRIMARY KEY (`SEQ_NAME`)
@@ -234,7 +279,7 @@ CREATE TABLE `sequence` (
 --
 
 INSERT INTO `sequence` (`SEQ_NAME`, `SEQ_COUNT`) VALUES
-('SEQ_GEN', 1751);
+('SEQ_GEN', '1801');
 
 -- --------------------------------------------------------
 
@@ -242,7 +287,7 @@ INSERT INTO `sequence` (`SEQ_NAME`, `SEQ_COUNT`) VALUES
 -- Structure de la table `utilisateur`
 --
 
-CREATE TABLE `utilisateur` (
+CREATE TABLE IF NOT EXISTS `utilisateur` (
   `login` varchar(20) NOT NULL,
   `pass` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`login`)
@@ -254,3 +299,7 @@ CREATE TABLE `utilisateur` (
 
 INSERT INTO `utilisateur` (`login`, `pass`) VALUES
 ('moussa', '0cc85dab9975ab9f00c966a9666df97e');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
