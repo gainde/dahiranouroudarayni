f77b26422cea69b9c28bd8ad857848fa0e0abe60
gainde <moussa.thimbo@gmail.com>
2014-06-21 16:51:35 -0400

    changement css dahira

--- src/logique/Main.java
+++ src/logique/Main.java
@@ -1,8 +1,5 @@
 package logique;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
 import javafx.application.Application;
 import javafx.home.Home;
 
@@ -10,24 +7,7 @@ public class Main {
 	
 
 	public static  void main(String args[]){
-		StringBuffer output = new StringBuffer();
-		Process p;
-		try {
-		p = Runtime.getRuntime().exec("/Applications/MAMP/bin/start.sh");
-			p.waitFor();
-			BufferedReader reader = 
-                    new BufferedReader(new InputStreamReader(p.getInputStream()));
-
-                String line = "";			
-                while ((line = reader.readLine())!= null) {
-                	output.append(line + "\n");
-                }
-                System.out.println(output);
 			Application.launch(Home.class);
- 
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
 	}
 	
 }
